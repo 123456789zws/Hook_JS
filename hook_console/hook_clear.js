951a497a75d361e2a33928919e3a9f2db6a4f264
0xsdeo <i0xsdeo@foxmail.com>
2024-12-17 23:31:16 +0800

    add hook_log v0.2

--- hook_console/hook_clear.js
+++ hook_console/hook_clear.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         hook_clear
-// @namespace    http://tampermonkey.net/
+// @namespace    https://github.com/0xsdeo/Hook_JS
 // @version      2024-11-09
-// @description  try to take over the world!
+// @description  禁止js清除控制台数据
 // @author       0xsdeo
 // @match        http://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
--- hook_console/hook_log v0.1.js
+++ hook_console/hook_log v0.1.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         hook_log
-// @namespace    http://tampermonkey.net/
+// @name         hook_log v0.1
+// @namespace    https://github.com/0xsdeo/Hook_JS
 // @version      2024-12-02
-// @description  Prevent overwriting log methods
+// @description  通过冻结console对象来禁止js重写log方法
 // @author       0xsdeo
 // @match        http://*/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
