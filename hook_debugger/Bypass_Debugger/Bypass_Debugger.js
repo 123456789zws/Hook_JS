b4081a86356fbc449f10a8d749eab997f2ceeec3
0xsdeo <i0xsdeo@foxmail.com>
2024-12-31 23:25:03 +0800

    Update Bypass_Debugger.js

--- hook_debugger/Bypass_Debugger/Bypass_Debugger.js
+++ hook_debugger/Bypass_Debugger/Bypass_Debugger.js
@@ -12,50 +12,35 @@
 (function() {
     'use strict';
 
-    var temp_eval = eval;
-    var temp_toString = Function.prototype.toString;
+    let temp_eval = eval;
+    let temp_toString = Function.prototype.toString;
 
     Function.prototype.toString = function () {
         if (this === eval) {
             return 'function eval() { [native code] }';
         } else if (this === Function) {
             return 'function Function() { [native code] }';
-        }
-        else if (this === Function.prototype.toString) {
+        } else if (this === Function.prototype.toString) {
             return 'function toString() { [native code] }';
+        } else if (this === Function.prototype.constructor) {
+            return 'function Function() { [native code] }';
         }
         return temp_toString.apply(this, arguments);
     }
 
     window.eval = function () {
         if (typeof arguments[0] == "string") {
-            var temp_length = arguments[0].match(/debugger/g);
-            if (temp_length != null) {
-                temp_length = temp_length.length;
-                var reg = /debugger/;
-                while (temp_length) {
-                    arguments[0] = arguments[0].replace(reg, "");
-                    temp_length--;
-                }
-            }
+            arguments[0] = arguments[0].replaceAll(/debugger/g, '');
         }
         return temp_eval(...arguments);
     }
 
-    var Bypass_debugger = Function;
+    let Bypass_debugger = Function;
 
     Function = function () {
-        var reg = /debugger/;
-        for (var i = 0; i < arguments.length; i++) {
+        for (let i = 0; i < arguments.length; i++) {
             if (typeof arguments[i] == "string") {
-                var temp_length = arguments[i].match(/debugger/g);
-                if (temp_length != null) {
-                    temp_length = temp_length.length;
-                    while (temp_length) {
-                        arguments[i] = arguments[i].replace(reg, "");
-                        temp_length--;
-                    }
-                }
+                arguments[i] = arguments[i].replaceAll(/debugger/g, '');
             }
         }
         return Bypass_debugger(...arguments);
@@ -64,17 +49,9 @@
     Function.prototype = Bypass_debugger.prototype;
 
     Function.prototype.constructor = function () {
-        var reg = /debugger/;
-        for (var i = 0; i < arguments.length; i++) {
+        for (let i = 0; i < arguments.length; i++) {
             if (typeof arguments[i] == "string") {
-                var temp_length = arguments[i].match(/debugger/g);
-                if (temp_length != null) {
-                    temp_length = temp_length.length;
-                    while (temp_length) {
-                        arguments[i] = arguments[i].replace(reg, "");
-                        temp_length--;
-                    }
-                }
+                arguments[i] = arguments[i].replaceAll(/debugger/g, '');
             }
         }
         return Bypass_debugger(...arguments);
